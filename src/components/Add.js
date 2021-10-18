2de68cf0ecf7509c235d5e5cfad568d1670ad350
i-m-ranjeet <mrranjeet11115@gmail.com>
2021-10-18 15:49:53 +0530

    some update

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,17 +1,45 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
-function Add({newEntry}) {
-    const [data,setData]=useState([])
+function Add({newEntry, update, letedit, oldvalue}) {
+    const [data,setData]=useState([]);
+    const [letEDIT,setLetEdit]=useState(letedit);
+    // const [old,setOld]=useState(oldvalue);
+
+
     const handleChange=(e)=>{
         setData(e.target.value);
     }
+
+
     const addNew=()=>{
         newEntry(data);
+        setData("");
     }
+    const No=()=>{
+        setData("")
+    }
+    
+    const editOld=()=>{
+        update(data);
+        setLetEdit(!letEDIT)
+        // setData("");
+        No();
+    }
+
+
+    useEffect(() => {
+        setData(oldvalue);
+        setLetEdit(!letedit)
+    }, [oldvalue,letedit]);
+
+
+    const add = <label htmlFor="Add"  onClick={addNew} >Add to List</label>;
+    const edit = <label htmlFor="Add" className="editbtn" onClick={editOld} >Edit in List</label>;
+
     return (
         <div className="add">
-            <input type="text" name="" placeholder="Enter Here..." onChange={handleChange} />
-            <label htmlFor="Add" onClick={addNew} >Add to List</label>
+            <input type="text" name="" placeholder="Enter Here..." /*value={letedit?data:data}*/ value={data} onChange={handleChange} />
+            {!letEDIT ? edit : add}
         </div>
     )
 }
--- src/components/View.js
+++ src/components/View.js
@@ -11,13 +11,15 @@ function View() {
 ///////   Creating States      ///////////
   const [state,setState]=useState([]);
   const [deleted,setDeleted]=useState(true);
+  const [key,setKey]=useState();
+  const [letedit,setLetEdit]=useState(false);
+  const [oldvalue,setOldValue]=useState();
 
 
 ///////   Getting Data From    //////////
   const getInitialData = ()=>{
     axios.get("http://5.189.130.81:1337/restaurants").then((res)=>{
       setState([...res.data]);
-      // console.log(state);
     });
   }
 
@@ -28,22 +30,34 @@ function View() {
     setDeleted(!deleted);
   }
 
-
+////////// Getting Key for update API value  /////////
   const updateItem=(e)=>{
-    const update={name:"Ranjeet"};
-    axios.put(`http://5.189.130.81:1337/restaurants/${e.target.parentElement.firstElementChild.innerText}`,update)
+    const getkey = e.target.parentElement.firstElementChild.innerText;
+    const value = e.target.parentElement.childNodes[1].innerText;
+    setOldValue(value);
+    // e.target.parentElement.parentElement.parentElement.classList.toggle("active")
+    // console.log(e.target.parentElement.parentElement.parentElement)
+    setKey(getkey);
+    setLetEdit(true)
+  }
+
+//////////////  Updating exist value //////////////////
+  const update=(newupdate)=>{
+    const update={name:newupdate};
+    axios.put(`http://5.189.130.81:1337/restaurants/${key}`,update);
     setDeleted(!deleted);
+    setLetEdit(false);
   }
 
 
-////////  Getting Data from ./Add.js component And Posting to API    //////
+/////////////    Getting Data from ./Add.js component And Posting to API    //////
   const newEntry=(gettingnew)=>{
     const newpayload={name:gettingnew};
     axios.post("http://5.189.130.81:1337/restaurants",newpayload);
   }
 
 
-/////// Calling useEffect Whwnever Data Added, Deleted and Updated in API //////
+//////////  Calling useEffect Whenever Data Added, Deleted and Updated in API    ////////
   useEffect(() => {
     getInitialData()
   }, [state,deleted]);
@@ -51,7 +65,7 @@ function View() {
 
     return (
         <div className="container">
-          <Add newEntry={newEntry} />
+          <Add newEntry={newEntry}  update={update} letedit={letedit} oldvalue={oldvalue} />
           <table cellSpacing="5px">
             <tbody>
             <tr>
